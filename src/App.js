81668edecce849b411478c624260ffac4ba6b51f
Den Pol <selection4test@gmail.com>
2019-07-24 12:22:52 +0300

    Started with react-to-print

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactToPrint from 'react-to-print';
+
+import ComponentToPrint from './components/ComponentToPrint';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+        <div>
+          <ReactToPrint
+            trigger={() => <a href="#">Print this out!</a>}
+            content={() => this.componentRef}
+          />
+          <ComponentToPrint ref={el => (this.componentRef = el)} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
