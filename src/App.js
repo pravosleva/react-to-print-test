29177fdd909e56427f01faee06615e6185077f0f
Den Pol <selection4test@gmail.com>
2019-07-24 14:59:11 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -18,7 +18,12 @@ class App extends React.Component {
             trigger={() => <a href="#">Print this out!</a>}
             content={() => this.componentRef}
           />
-          <ComponentToPrint ref={el => (this.componentRef = el)} />
+          <ComponentToPrint
+            ref={el => (this.componentRef = el)}
+            x={1}
+            y={2}
+            z={3}
+          />
         </div>
       </div>
     );
--- src/components/ComponentToPrint.js
+++ src/components/ComponentToPrint.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 class ComponentToPrint extends React.Component {
   render() {
+    const { x, y, z } = this.props;
+
     return (
       <table>
         <thead>
-          <th>column 1</th>
-          <th>column 2</th>
-          <th>column 3</th>
+          <th>x</th>
+          <th>y</th>
+          <th>z</th>
         </thead>
         <tbody>
           <tr>
-            <td>data 1</td>
-            <td>data 2</td>
-            <td>data 3</td>
+            <td>{x}</td>
+            <td>{y}</td>
+            <td>{z}</td>
           </tr>
           <tr>
             <td>data 1</td>
